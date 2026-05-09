37ae3841ef3ed125f49be4c9351c72a87791a831
Marten Lienen <m@mrtn.name>
2026-05-09 12:14:26 +0200

    Add some more information to the commentary section

--- emcp.el
+++ emcp.el
@@ -23,7 +23,10 @@
 
 ;;; Commentary:
 
-;; EMCP lets you connect your LLM agent directly to Emacs through an MCP server.
+;; EMCP lets you connect your LLM agent directly to Emacs through an MCP server.  The
+;; agent can look up documentation and definitions, take screenshots, read buffers,
+;; execute code and more.  Exactly which prompts, resources and tools are available to the
+;; agent depends on the active profile.
 
 ;;; Code:
 
